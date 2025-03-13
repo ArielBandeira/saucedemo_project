10003f66a81018905f2413e0b997e7d101e0a625
Ariel <arieltwain@gmail.com>
2025-03-12 22:09:27 -0300

    adding command to go to checkout page with items

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -25,15 +25,41 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 /// <reference types="../support" />
 
+import {HomePage} from "./pageObjects/HomePage";
+import {CartPage} from "./pageObjects/CartPage";
+import {CheckoutStepOnePage} from "./pageObjects/CheckoutStepOnePage";
+
 Cypress.Commands.add('loginUiValidUser', () => {
     let validUsername: string, validPassword: string;
     cy.visit('https://www.saucedemo.com/v1/');
     cy.fixture('users.json').then((data) => {
         validUsername = data.validUser.username;
         validPassword = data.validUser.password;
+
         cy.get('[data-test="username"]').type(validUsername);
         cy.get('[data-test="password"]').type(validPassword);
     });
     cy.get('[type="submit"]').click();
     cy.url().should('include', '/inventory.html');
+});
+
+Cypress.Commands.add('goToCheckoutPageTwoWithItems', (qtd: number) => {
+    const homePage = new HomePage();
+    const cartPage = new CartPage();
+    const checkoutStepTwoPage = new CheckoutStepOnePage();
+    let firstName: string, lastName: string, postalCode: string;
+
+    for (let i = 0; i < qtd; i++) {
+        homePage.addFirstItemToCart();
+    }
+
+    homePage.goToCartPage();
+    cartPage.clickCheckout();
+    cy.fixture('users.json').then((data) => {
+        firstName = data.validUser.firstName;
+        lastName = data.validUser.lastName;
+        postalCode = data.validUser.postalCode;
+        checkoutStepTwoPage.fillCheckoutForm(firstName, lastName, postalCode);
+    });
+    checkoutStepTwoPage.clickContinueButton();
 });
