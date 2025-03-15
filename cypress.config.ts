ce45fc566a29a3ba842771ff96ade4ea793b837a
Ariel <arieltwain@gmail.com>
2025-03-15 00:21:59 -0300

    adding Mochawesome reports

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     // Configure your E2E tests here
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
     screenshotOnRunFailure: true,
-    screenshotsFolder: "cypress/screenshots"
+    screenshotsFolder: "cypress/screenshots",
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    }
   },
 })
